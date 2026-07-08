871b890f26dbef7c7e0c56b1941e6018f254ef42
Clément Antoine XAVIER <clement-antoine.xavier@epitech.eu>
2026-07-08 23:35:48 +0900

    Feat: Add react-native-paper and friends page

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,4 +1,5 @@
 import { Tabs } from 'expo-router';
+import { MaterialDesignIcons } from "@react-native-vector-icons/material-design-icons";
 
 export default function TabsLayout() {
   return (
@@ -7,24 +8,40 @@ export default function TabsLayout() {
         name="(news)"
         options={{
           title: 'News',
+          tabBarIcon: ({ focused, color, size, }) =>
+            <MaterialDesignIcons name={focused ? "newspaper-variant" : "newspaper-variant-outline"} color={color} size={size} />
         }}
       />
       <Tabs.Screen
         name="discover"
         options={{
           title: 'Discover',
+          tabBarIcon: ({ focused, color, size, }) =>
+            <MaterialDesignIcons name={focused ? "compass" : "compass-outline"} color={color} size={size} />
+        }}
+      />
+      <Tabs.Screen
+        name="friends"
+        options={{
+          title: 'Friends',
+          tabBarIcon: ({ focused, color, size, }) =>
+            <MaterialDesignIcons name={focused ? "account-multiple" : "account-multiple-outline"} color={color} size={size} />
         }}
       />
       <Tabs.Screen
         name="communities"
         options={{
           title: 'Communities',
+          tabBarIcon: ({ focused, color, size, }) =>
+            <MaterialDesignIcons name={focused ? "account-group" : "account-group-outline"} color={color} size={size} />
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: 'Profile',
+          tabBarIcon: ({ focused, color, size, }) =>
+            <MaterialDesignIcons name={focused ? "account" : "account-outline"} color={color} size={size} />
         }}
       />
     </Tabs>
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,20 +1,42 @@
-import { ThemeProvider, DarkTheme, DefaultTheme, Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useColorScheme } from 'react-native';
+import {
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+  ThemeProvider,
+  Stack,
+} from 'expo-router';
+import {
+  MD3DarkTheme,
+  MD3LightTheme,
+  adaptNavigationTheme,
+  PaperProvider,
+} from 'react-native-paper';
+import merge from 'deepmerge';
 
 SplashScreen.preventAutoHideAsync();
+
+const { LightTheme, DarkTheme } = adaptNavigationTheme({
+  reactNavigationLight: NavigationDefaultTheme,
+  reactNavigationDark: NavigationDarkTheme,
+});
+
+const CombinedDefaultTheme = merge(MD3LightTheme, LightTheme);
+const CombinedDarkTheme = merge(MD3DarkTheme, DarkTheme);
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="auth/sign-up" options={{ headerShown: false }} />
-        <Stack.Screen name="auth/sign-in" options={{ headerShown: false }} />
-        <Stack.Screen name="auth/reset-password" options={{ headerShown: false }} />
-      </Stack>
-    </ThemeProvider>
+    <PaperProvider theme={colorScheme === 'dark' ? CombinedDarkTheme : CombinedDefaultTheme}>
+      <ThemeProvider value={colorScheme === 'dark' ? CombinedDarkTheme : CombinedDefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="auth/sign-up" options={{ headerShown: false }} />
+          <Stack.Screen name="auth/sign-in" options={{ headerShown: false }} />
+          <Stack.Screen name="auth/reset-password" options={{ headerShown: false }} />
+        </Stack>
+      </ThemeProvider>
+    </PaperProvider>
   );
 }
